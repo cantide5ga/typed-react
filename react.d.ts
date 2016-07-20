360b07656db2b254fe9c629e1c162970f8cf562c
Michael Neil Payne <thruston82@rocketmail.com>
2016-07-20 14:18:02 -0400

    issue#2 added persist fn signature to SyntheticEvent

--- react.d.ts
+++ react.d.ts
@@ -275,6 +275,7 @@ declare namespace React {
         nativeEvent: Event;
         preventDefault(): void;
         stopPropagation(): void;
+        persist(): void;
         target: EventTarget;
         timeStamp: Date;
         type: string;
